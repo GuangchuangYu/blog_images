248b73533d2ba17eb666314141c807d082236dae
guangchuang yu <guangchuangyu@gamil.com>
2017-04-15 16:22:19 +0800

    biobabble_banner

--- biobabble_banner.R
+++ biobabble_banner.R
@@ -8,7 +8,6 @@ page <- rasterGrob(image=readImage('biobabble_page.png'))
 
 pg <- ggplot(d=data.frame(x=c(0,10), y=c(0,5)), aes(x, y)) + geom_blank() + coord_fixed()
 
-pg + annotation_custom(raster(p), xmin=5, xmax=10, ymin=1, ymax=5)
 
 
 pg <- pg+geom_subview(gc, x=2.5, y=2.5, width=5.1, height=5.1) + geom_subview(page, x=7.5, y=2.5, width=4.8, height=4.8)
@@ -19,3 +18,9 @@ pg <- pg+theme_void() + labs(title="长按二维码关注biobabble", subtitle="�
                              caption='guangchuangyu@gmail.com') +
     theme(title=element_text(family="heiti", size=20))
 ggsave(pg, file="biobabble_banner.png", width=8, height=5)
+
+
+## for qrcode
+
+require(qrcode)
+qrcode_gen('https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzI5NjUyNzkxMg==&scene=124#wechat_redirect')
